c32e81a9cb2c452f880c23766064a616d2cbfea5
Matt Murphy <matt@mattmurphy.ca>
2019-04-08 19:28:42 -0400

    add new playlist to user in database
    also show playlist successfully created message with a link to playlist

--- client/src/pages/NewPlaylist.jsx
+++ client/src/pages/NewPlaylist.jsx
@@ -20,7 +20,7 @@ class NewPlaylist extends Component {
 	}
 	async handleSubmit(e) {
 		e.preventDefault()
-		const name = e.target['playlist-name'].value
+		const name = e.target['playlist-name'].value.trim()
 		const songs = {}
 		Object.keys(this.state.playlist.songs).forEach((song) => {
 			songs[song] = {}
@@ -31,21 +31,27 @@ class NewPlaylist extends Component {
 
 		if (Object.values(this.state.playlist.songs).length === 0) {
 			this.setState({ error: 'Error: Playlist must contain at least one track.' })
+		} else if (name.length < 1) {
+			this.setState({ error: 'Error: Playlist must have a name.' })
 		} else {
 			e.target['playlist-name'].value = ''
 
-			let index
-			await fetch('http://localhost/api/playlists/last-index')
-				.then((res) => res.json())
-				.then((res) => (index = res + 1))
-			const playlist = { name, songs, index }
+			const playlist = { name, songs }
 			console.log('adding', playlist)
 
-			fetch('http://localhost/api/playlists', {
+			const { account_id } = await this.props.dbx.usersGetCurrentAccount()
+			fetch(`http://localhost/api/${account_id}/playlists`, {
 				method: 'POST',
 				body: JSON.stringify(playlist),
 				headers: { 'Content-Type': 'application/json' }
-			}).then((res) => console.log(res))
+			}).then(async (res) => {
+				const { _id } = await res.json()
+				console.log(res)
+				this.setState({
+					added: true,
+					link: `/playlists/${_id}`
+				})
+			})
 
 			if (this.state.error) this.setState({ error: undefined })
 		}
@@ -63,6 +69,11 @@ class NewPlaylist extends Component {
 			/>
 			<form id="create-playlist" onSubmit={(e) => this.handleSubmit(e)}>
 				{this.state.error && this.error()}
+				{this.state.added && (
+					<p>
+						Playlist created! Listen to it <a href={this.state.link}>here</a>.
+					</p>
+				)}
 				<input type="text" name="playlist-name" placeholder="Playlist Title" />
 				<button>Create playlist</button>
 			</form>
