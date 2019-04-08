bbfcf2c77668d9aa971e256f24bae4fac38f049b
Matt Murphy <matt@mattmurphy.ca>
2019-04-08 18:58:08 -0400

    pull updated routes

--- routes/api.js
+++ routes/api.js
@@ -1,67 +1,75 @@
 const router = require('express').Router()
 const User = require('../models/user')
 
-async function getLastPlaylistIndex(userIndex) {
-	let lastPlaylistIndex = 0
-	await User.findOne({ index: userIndex }, {}, { sort: { date: -1 } }).then(
-		(playlist) => (lastPlaylistIndex = playlist.index)
-	)
-	return lastPlaylistIndex
+async function getLastPlaylistId(userId) {
+	let lastPlaylistId = 0
+	await User.findById(userId).then(({ playlists }) => {
+		lastPlaylistId = playlists.length > 0 ? playlists[playlists.length - 1]._id : 0
+	})
+	return lastPlaylistId
 }
-
-async function getLastUserIndex() {
-	let lastUserIndex = 0
-	await User.findOne({}, {}, { sort: { date: -1 } }).then((user) => (lastUserIndex = user.index))
-	return lastUserIndex
-}
-
-// get playlists of user
-router.get('/:userId/playlists', (req, res, next) => {
-	User.findOne({ index: req.params.userId }).then((record) => res.json(record.playlists))
-})
 
 // create new playlist
 router.post('/:userId/playlists', async (req, res, next) => {
-	const userIndex = req.params.userId
-	const lastPlaylistIndex = await getLastPlaylistIndex(userIndex)
+	const userId = req.params.userId
+	const lastPlaylistId = await getLastPlaylistId(userId)
 	const playlist = req.body
-	playlist.index = lastPlaylistIndex + 1
+	playlist._id = Number(lastPlaylistId) + 1
 
-	User.findOne({ index: userIndex }).then((record) => {
-		record.playlists.push(playlist)
-		record.save()
-		res.json(record)
+	User.findById(userId).then((user) => {
+		user.playlists.push(playlist)
+		user.save()
+		res.json(playlist)
+	})
+})
+
+// delete a playlist
+router.delete('/:userId/playlists/:playlistId', ({ params }, res, next) => {
+	const userId = params.userId
+	const playlistId = params.playlistId
+	User.updateOne({ _id: userId }, { $pull: { playlists: { _id: playlistId } } })
+		.then((data) => res.json(data))
+		.catch(next)
+})
+
+// replace playlist: PUT
+router.put('/:userId/playlists/:playlistId', (req, res, next) => {
+	const userId = req.params.userId
+	const playlistId = req.params.playlistId
+	const newPlaylist = req.body
+	newPlaylist._id = playlistId
+	User.findById(userId).then((user) => {
+		const oldPlaylist = user.playlists.id(playlistId).remove()
+		user.playlists.push(newPlaylist)
+		user.save()
+	})
+})
+
+// get all playlists of user
+router.get('/:userId/playlists', ({ params }, res, next) => {
+	User.findById(params.userId).then((user) => res.json(user.playlists))
+})
+
+// get a playlist by its id
+router.get('/:userId/playlists/:playlistId', ({ params }, res, next) => {
+	const userId = params.userId
+	const playlistId = params.playlistId
+	User.findById(userId).then((user) => {
+		const playlist = user.playlists.id(playlistId)
+		res.json(playlist)
 	})
 })
 
 // create new user
-router.post('/users', async (req, res, next) => {
-	// add indexes to each playlist
-	if (req.body.playlists) {
-		req.body.playlists = req.body.playlists.map((playlist, i) => {
-			playlist.index = i + 1
+router.post('/users', async ({ body }, res, next) => {
+	// add IDs to each playlist
+	if (body.playlists) {
+		body.playlists = body.playlists.map((playlist, i) => {
+			playlist._id = i + 1
 			return playlist
 		})
 	}
-	// add unused index to user
-	req.body.index = (await getLastUserIndex()) + 1
-	User.create(req.body).then((data) => res.json(data)).catch((err) => console.log(err))
-})
-
-// get a playlist by its id
-router.get('/:userId/playlists/:playlistId', (req, res, next) => {
-	const userIndex = req.params.userId
-	const playlistIndex = req.params.playlistId
-	User.findOne({ index: userIndex, 'playlists.index': playlistIndex }).then((record) => res.json(record))
-})
-
-// delete a playlist
-router.delete('/:userId/playlists/:playlistId', (req, res, next) => {
-	const userIndex = req.params.userId
-	const playlistIndex = req.params.playlistId
-	User.updateOne({ index: userIndex }, { $pull: { playlists: { index: playlistIndex } } })
-		.then((data) => res.json(data))
-		.catch(next)
+	User.create(body).then((data) => res.json(data)).catch((err) => console.log(err))
 })
 
 module.exports = router
