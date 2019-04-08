f8da49fee268afa3f0f439d7cc01c7bef936af49
Matt Murphy <matt@mattmurphy.ca>
2019-04-07 23:17:55 -0400

    add support for running in production mode

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const routes = require('./routes/api')
+const path = require('path')
 require('dotenv').config()
 
 const port = process.env.PORT || 80
@@ -14,15 +16,29 @@ mongoose
 // override decprecated promise
 mongoose.Promise = global.Promise
 
+// must remain at the top for cross-origin requests to be allowed
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*')
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 	next()
 })
 
-app.use(bodyParser.json())
-
+// must remain above catch-all for other routes below which are all sent to index, to be handled by react router
 app.use('/api', routes)
+
+// production mode
+if (process.env.NODE_ENV === 'production') {
+	console.log('production')
+	app.use(express.static(path.join(__dirname, 'client/build')))
+	app.get('*', (req, res) => res.sendFile('client/build/index.html', { root: __dirname }))
+}
+
+// build mode
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
+
+app.use(bodyParser.json())
 
 app.use((err, req, res, next) => {
 	console.log(err)
