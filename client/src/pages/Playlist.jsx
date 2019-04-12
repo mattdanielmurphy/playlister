d1147362c005c5bafdb9adab75ca335bfc7937e3
Matt Murphy <matt@mattmurphy.ca>
2019-04-12 05:03:18 -0400

    Remove Safari alert
    Issue no longer applies (?)

--- client/src/pages/Playlist.jsx
+++ client/src/pages/Playlist.jsx
@@ -1,11 +1,7 @@
 import React, { Component } from 'react'
 import fetch from 'node-fetch'
-import Cookies from 'js-cookie'
-import { confirmAlert } from 'react-confirm-alert'
 
-import dropbox from '../components/dropbox'
 import Player from '../components/Player'
-import SafariAlert from '../components/SafariAlert'
 import Loader from '../components/Loader'
 
 class Playlist extends Component {
@@ -20,56 +16,31 @@ class Playlist extends Component {
 		fetch(`http://localhost/api/playlists/${this.state.id}`)
 			.then(async (res) => {
 				const { songs, name, date } = await res.json()
-				this.setState({
-					songs: Object.values(songs),
-					name,
-					date
-				})
+				this.setState({ songs: Object.values(songs), name, date })
 			})
-			.catch(() => {
-				this.setState({
-					error: 'Error: Playlist not found!'
-				})
-			})
-	}
-	alertAboutSafari() {
-		// only alert if user didn't request to not be reminded
-		if (!Cookies.get('doNotAlertAboutSafari')) {
-			const isSafari =
-				navigator.vendor &&
-				navigator.vendor.indexOf('Apple') > -1 &&
-				navigator.userAgent &&
-				navigator.userAgent.indexOf('CriOS') === -1 &&
-				navigator.userAgent.indexOf('FxiOS') === -1
-			if (isSafari) confirmAlert({ customUI: ({ onClose }) => <SafariAlert onClose={onClose} /> })
-		}
+			.catch(() => this.setState({ error: 'Error: Playlist not found!' }))
 	}
 	setPlaylistState = (state) => this.setState(state)
-	componentDidMount() {
-		this.getSongs()
-		this.alertAboutSafari()
-	}
-	render() {
-		return (
-			<main>
-				{this.state.error ? (
-					<h1>{this.state.error}</h1>
-				) : !this.state.songs || this.state.songs.length === 0 ? (
-					<Loader />
-				) : (
-					<div>
-						<h1>{this.state.name}</h1>
-						<Player
-							currentSongIndex={this.state.currentSongIndex}
-							songs={this.state.songs}
-							setCurrentSongIndex={(i) => this.setCurrentSongIndex(i)}
-							setPlaylistState={(state) => this.setPlaylistState(state)}
-						/>
-					</div>
-				)}
-			</main>
-		)
-	}
+	componentWillMount = () => this.getSongs()
+	render = () => (
+		<main>
+			{this.state.error ? (
+				<h1>{this.state.error}</h1>
+			) : !this.state.songs || this.state.songs.length === 0 ? (
+				<Loader />
+			) : (
+				<div>
+					<h1>{this.state.name}</h1>
+					<Player
+						currentSongIndex={this.state.currentSongIndex}
+						songs={this.state.songs}
+						setCurrentSongIndex={(i) => this.setCurrentSongIndex(i)}
+						setPlaylistState={(state) => this.setPlaylistState(state)}
+					/>
+				</div>
+			)}
+		</main>
+	)
 }
 
 export default Playlist
